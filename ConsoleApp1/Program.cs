90228972de18b34dda8b524dd10245331d6c0b31
abdugofurjon05 <gofurjonshuxratov05@gmail.com>
2025-04-11 10:43:21 +0500

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,3 +17,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
